a66625172a4a43071db16d24be7e2634b5cb482c
Mojtaba Rahimy <mojtaba.rahimy@chmail.ir>
2018-11-23 14:04:29 +0330

    init

---
+++ app/src/main/java/ir/mrahimy/ingress/portal/app/App.java
@@ -0,0 +1,13 @@
+package ir.mrahimy.ingress.portal.app;
+
+import android.app.Application;
+
+/**
+ * Created by vincent on 11/16/18.
+ */
+
+public class App extends Application {
+
+    //TODO: add double sync
+    //TODO: make server
+}
---
+++ app/src/main/java/ir/mrahimy/ingress/portal/view/MainActivity.kt
@@ -0,0 +1,57 @@
+package ir.mrahimy.ingress.portal.view
+
+import android.net.Uri
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import ir.mrahimy.ingress.portal.R
+import ir.mrahimy.ingress.portal.fragments.AddPortalFragment
+import ir.mrahimy.ingress.portal.fragments.HomeFragment
+import ir.mrahimy.ingress.portal.fragments.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),
+        HomeFragment.OnFragmentInteractionListener,
+        SettingsFragment.OnFragmentInteractionListener,
+        AddPortalFragment.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                //todo: portals list
+                goto(HomeFragment.newInstance("A", "B"))
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_add_portal -> {
+                //TODO: add portals
+                goto(AddPortalFragment.newInstance("A", "B"))
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_settings -> {
+                // TODO: settings
+                goto(SettingsFragment.newInstance("A", "B"))
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        goto(HomeFragment.newInstance("A", "B"))
+    }
+
+    fun goto(fragment: Fragment) {
+        val b = supportFragmentManager.beginTransaction()
+        b.replace(R.id.main_fragment_layout, fragment)
+        b.commit()
+    }
+}
